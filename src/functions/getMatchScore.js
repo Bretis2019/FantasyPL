7be7cb4e7e27907f671f7f700116398a43c130db
Bretis2019 <mosbahiwalid10@gmail.com>
2023-11-14 20:59:25 +0100

    added cron job and match scores

--- src/functions/getMatchScore.js
+++ src/functions/getMatchScore.js
@@ -2,5 +2,5 @@ export async function GetMatchScore(id){
     const url = `https://www.fotmob.com/api/matchDetails?matchId=${id}`;
     const response = await fetch(url);
     const data = await response.json();
-    const score = [data.header.teams[0].score, data.header.teams[1].score];
+    return [data.header.teams[0].score, data.header.teams[1].score];
 }
