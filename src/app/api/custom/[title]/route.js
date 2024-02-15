e68f9f277c4ec667d7cac2e4e09c9b7723de1cbb
Bretis2019 <mosbahiwalid10@gmail.com>
2024-02-15 11:45:36 +0100

    refactored function

--- src/app/api/custom/[title]/route.js
+++ src/app/api/custom/[title]/route.js
@@ -1,27 +1,29 @@
+async function getResponse(title){
+    const myHeaders = new Headers();
+    myHeaders.append(
+        "Ocp-Apim-Subscription-Key",
+        "355f21faaf944257a6c16278e917126f",
+    );
+
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+    };
+
+    fetch(`https://api.immigration.govt.nz/ea-datashare/v1/search/${title}?page=1&_=170795`, requestOptions,)
+        .then((response) => response.json())
+        .then((result) => {
+            return result.items.length > 0;
+        })
+        .catch((error) => {
+            return error;
+        })
+}
+
 export async function GET(request, { params }) {
-    try{
-        const { title } = params;
-        const myHeaders = new Headers();
-        myHeaders.append(
-            "Ocp-Apim-Subscription-Key",
-            "355f21faaf944257a6c16278e917126f",
-        );
+    const { title } = params;
+    const response = await getResponse(title);
+    return new Response(response);
 
-        const requestOptions = {
-            method: "GET",
-            headers: myHeaders,
-            redirect: "follow",
-        };
-
-        fetch(`https://api.immigration.govt.nz/ea-datashare/v1/search/${title}?page=1&_=170795`, requestOptions,)
-            .then((response) => response.json())
-            .then((result) => {
-                return new Response(result.items.length > 0)
-            })
-            .catch((error) => {
-                return new Response(error);
-            })
-    }catch(err){
-        return new Response(err)
-    }
 }
