d7cef6f53f777355c5d7d859ab79151f58edc2e5
Bretis2019 <mosbahiwalid10@gmail.com>
2024-02-15 18:02:31 +0100

    Finally fixed it

--- src/app/api/custom/[title]/route.js
+++ src/app/api/custom/[title]/route.js
@@ -1,29 +1,28 @@
-async function getResponse(title){
-    const myHeaders = new Headers();
-    myHeaders.append(
-        "Ocp-Apim-Subscription-Key",
-        "355f21faaf944257a6c16278e917126f",
-    );
-
-    const requestOptions = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow",
-    };
-
-    fetch(`https://api.immigration.govt.nz/ea-datashare/v1/search/${title}?page=1&_=170795`, requestOptions,)
-        .then((response) => response.json())
-        .then((result) => {
-            return result.items.length > 0;
-        })
-        .catch((error) => {
-            return error;
-        })
-}
-
 export async function GET(request, { params }) {
     const { title } = params;
-    const response = await getResponse(title);
-    return new Response(response);
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append(
+            "Ocp-Apim-Subscription-Key",
+            "355f21faaf944257a6c16278e917126f",
+        );
 
+        const requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow",
+        };
+        // Fetch data from your API using the title
+        const response = await fetch(`https://api.immigration.govt.nz/ea-datashare/v1/search/${title}?page=1&_=170795`, requestOptions);
+        const data = await response.json();
+
+        // Check if the response data has a non-empty items array
+        const hasItems = Array.isArray(data.items) && data.items.length > 0;
+
+        // Return true if items array is not empty, otherwise return false
+        return new Response(hasItems);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return new Response({ error: 'Error fetching data' });
+    }
 }
